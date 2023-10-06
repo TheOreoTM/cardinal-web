4265511f258d653f0c15a1f117e95bbc29b218a6
TheOreoTM <qwertzybad@gmail.com>
2023-10-07 02:32:01 +0500

    BRO?

--- src/app/dashboard/[guildId]/logging/page.tsx
+++ src/app/dashboard/[guildId]/logging/page.tsx
@@ -3,11 +3,25 @@ import { Heading } from "@/components/ui/Heading";
 import { Textarea } from "@/components/ui/TextArea";
 import { getGuild } from "../../loaders";
 import { redirect } from "next/navigation";
+import axios from "axios";
+import type { APIChannel } from "discord-api-types/v9";
 
 async function getCurrentGuild(guildId: string) {
 	const guild = await getGuild(guildId);
 	if (!guild) return redirect("/dashboard");
 	return guild;
+}
+
+async function getChannels(guildId: string) {
+	const result = await axios<APIChannel[]>({
+		url: `/api/discord/getChannels?guildId=${guildId}`,
+		method: "GET",
+	}).catch((e) => console.error(e));
+
+	const channels = result!.data.filter(
+		(channel: APIChannel) => channel.type === 0
+	);
+	return channels;
 }
 
 export default async function Logging({
@@ -16,6 +30,7 @@ export default async function Logging({
 	params: { guildId: string };
 }) {
 	const guild = await getCurrentGuild(params.guildId);
+	const channels = await getChannels(guild.id);
 
 	return (
 		<Shell layout="dashboard">
@@ -24,7 +39,7 @@ export default async function Logging({
 				description="Change the logging settings of the server"
 				title={`Logging settings for ${guild.name}`}
 			/>
-			{JSON.stringify(guild)}
+			{JSON.stringify(channels)}
 			<Textarea
 				rows={1}
 				className="w-full p-2 border resize-none"
